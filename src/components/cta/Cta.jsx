aeb0cea9aa1ab4e084684b9347de4b2011421a60
joelkhamala <jwanyoa@gmail.com>
2022-12-22 11:45:54 +0300

    Added articles Features

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -3,7 +3,17 @@ import './cta.css'
 
 const Cta = () => {
   return (
-    <div>Cta</div>
+    <div className='modernui__cta'>
+      <div className='modernui__cta-content'>
+        <p>Request early access to get started</p>
+        <h3>Register today & start exploring endless possibilities</h3>
+      </div>
+      <div className='modernui__cta-btn'>
+        <button type='button'>
+          Get Started
+        </button>
+      </div>
+    </div>
   )
 }
 
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import './blog.css'
+import {Article } from '../../components';
+
+import { blog01, blog02, blog03, blog04, blog05 } from './imports'
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='modernui__blog section__padding' id='blog'>
+      <div className='modernui__blog-heading'>
+        <h1 className='gradient__text'>Alot is happeing, we are blogging about it</h1>
+      </div>
+      <div className='modernui__blog-container'>
+        <div className='modernui__blog-container_groupA'>
+          <Article imageUrl={ blog01 } date="Dec 22, 2022" title="ModernUI and OpenAI is the future. Le uts explore it."/>
+        </div>
+        <div className='modernui__blog-container_groupB'>
+        <Article imageUrl={ blog02 } date="Dec 22, 2022" title="ModernUI and OpenAI is the future. Le uts explore it."/>
+        <Article imageUrl={ blog03 } date="Dec 22, 2022" title="ModernUI and OpenAI is the future. Le uts explore it."/>
+        <Article imageUrl={ blog04 } date="Dec 22, 2022" title="ModernUI and OpenAI is the future. Le uts explore it."/>
+        <Article imageUrl={ blog05 } date="Dec 22, 2022" title="ModernUI and OpenAI is the future. Le uts explore it."/>
+        </div>
+      </div>
+    </div>
   )
 }
 
